8b40a7c32f5b3f9ed4faaa8d0dd5b1033942f84d
Bruno Nobrega <bruno.nobrega@hotmail.com>
2023-04-30 19:55:28 +0100

    Test map finished

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.3f;
+    [SerializeField] Color32 hasPackageColor = new Color32(255, 0, 0, 255);
+    [SerializeField] Color32 deliveredPackageColor = new Color32(255, 170, 0, 0);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+    bool hasPackage;
+    float colorDelay = 1f;
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ouch!");
@@ -11,14 +24,37 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.tag == "Package") 
+        if (other.tag == "Package" && !hasPackage) 
         {
             Debug.Log("Package pickd up");
+            hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
+
+            Destroy(other.gameObject, destroyDelay);
         }
         
-        if (other.tag == "Customer") 
+        if (other.tag == "Customer" && hasPackage) 
         {
             Debug.Log("Delivered package");
+            hasPackage = false;
+
+            spriteRenderer.color = deliveredPackageColor;
+
+            StartCoroutine(ChangeColorDelay(colorDelay, noPackageColor));
+            
         }
+
+        if (other.tag == "StartLine") 
+        {
+            Debug.Log("Timer running");
+        }
+    }
+
+    IEnumerator ChangeColorDelay(float time, Color32 color) 
+    {
+        Debug.Log(time);
+        yield return new WaitForSeconds(time);
+
+        spriteRenderer.color = color;
     }
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 250;
-    [SerializeField] float moveSpeed = 10;
-
-    void Start()
-    {
-        
-    }
+    [SerializeField] float steerSpeed = 250f;
+    [SerializeField] float defaultSpeed = 20f;
+    [SerializeField] float moveSpeed = 20f;
+    [SerializeField] float bumpSpeed = 15f;
+    [SerializeField] float boostSpeed = 25f;
 
     void Update()
+    {
+        // movement controller
+        InputController(steerSpeed, moveSpeed);
+
+        // speed perks
+        // SpeedPerks(bumpSpeed, boostSpeed);
+    }
+
+    void InputController(float steerSpeed, float moveSpeed) 
     {
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
 
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(0, moveAmount, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        if(moveSpeed > 20)
+        {
+            moveSpeed = defaultSpeed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+         if (other.tag == "BumpSpeed") 
+        {
+            Debug.Log("Speed decreased");
+            moveSpeed = bumpSpeed;
+        }
+        
+        if (other.tag == "BoostSpeed") 
+        {
+            Debug.Log("Speed increased");
+            moveSpeed = boostSpeed;
+        }
     }
 }
